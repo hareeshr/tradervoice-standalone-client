96b210ebd53df2591b901e491078b64b42e7fbf8
Hareesh Ramachandran <hareeshr.2007@gmail.com>
2023-07-01 13:57:53 +0400

    Intro added

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,56 +1,82 @@
-import { Paper, Stack, Typography } from '@mui/material';
+import React from 'react'
+import './Intro.css'
 
-type SectionProps = {
-  children: React.ReactNode;
-};
-
-const Section = (props: SectionProps) => (
-  <Paper
-    component="article"
-    sx={{
-      backgroundColor: (theme) => theme.palette.action.hover,
-      flex: '1 1 50%',
-      hyphens: 'auto',
-      p: 2,
-      pb: 0
-    }}
-  >
-    {props.children}
-  </Paper>
-);
-
-export default function Intro() {
+const Intro = () => {
   return (
-    <Stack
-      component="aside"
-      direction={{ xs: 'column', sm: 'row' }}
-      justifyContent="space-between"
-      alignItems="stretch"
-      spacing={2}
-    >
-      <Section>
-        <Typography textAlign="justify" variant="body1" paragraph={true}>
-          Tradervoice lets you <strong>search and discover news trends</strong> and relevant articles.
-        </Typography>
+    <div className="tv-inner-box">
 
-        <Typography textAlign="justify" variant="body1" paragraph={true}>
-          Your searches are based on thousands of news sources from around the world.
-        </Typography>
+      <h1>Welcome to TraderVoice.io</h1>
 
-        <Typography textAlign="justify" variant="body1" paragraph={true}>
-          We update our data every two hours to ensure that you receive the most current results.
-        </Typography>
-      </Section>
+      <p className="introText">
+        Tradervoice lets you <strong>search and discover news trends</strong> and relevant articles.<br /> 
+        Your searches are based on thousands of news sources from around the world.<br /> 
+        We update our data every two hours to ensure that you receive the most current results.<br /> 
+      </p>
 
-      <Section>
-        <Typography textAlign="justify" variant="body1" paragraph={true}>
-          By overlaying different ticker symbols, you can use Tradervoice to explore the relationship between your search and the financial markets and economy.
-        </Typography>
-
-        <Typography textAlign="justify" variant="body1" paragraph={true}>
-          You can download the results of your customized searches for further analysis.
-        </Typography>
-      </Section>
-    </Stack>
-  );
+      <p className="introText">
+        By overlaying different ticker symbols, you can use Tradervoice to explore the relationship between your search and the financial markets and economy.<br /> 
+        You can download the results of your customized searches for further analysis.
+      </p>
+      
+    </div>
+  )
 }
+
+export default Intro
+
+// import { Paper, Stack, Typography } from '@mui/material';
+
+// type SectionProps = {
+//   children: React.ReactNode;
+// };
+
+// const Section = (props: SectionProps) => (
+//   <Paper
+//     component="article"
+//     sx={{
+//       backgroundColor: (theme) => theme.palette.action.hover,
+//       flex: '1 1 50%',
+//       hyphens: 'auto',
+//       p: 2,
+//       pb: 0
+//     }}
+//   >
+//     {props.children}
+//   </Paper>
+// );
+
+// export default function Intro() {
+//   return (
+//     <Stack
+//       component="aside"
+//       direction={{ xs: 'column', sm: 'row' }}
+//       justifyContent="space-between"
+//       alignItems="stretch"
+//       spacing={2}
+//     >
+//       <Section>
+//         <Typography textAlign="justify" variant="body1" paragraph={true}>
+//           Tradervoice lets you <strong>search and discover news trends</strong> and relevant articles.
+//         </Typography>
+
+//         <Typography textAlign="justify" variant="body1" paragraph={true}>
+//           Your searches are based on thousands of news sources from around the world.
+//         </Typography>
+
+//         <Typography textAlign="justify" variant="body1" paragraph={true}>
+//           We update our data every two hours to ensure that you receive the most current results.
+//         </Typography>
+//       </Section>
+
+//       <Section>
+//         <Typography textAlign="justify" variant="body1" paragraph={true}>
+//           By overlaying different ticker symbols, you can use Tradervoice to explore the relationship between your search and the financial markets and economy.
+//         </Typography>
+
+//         <Typography textAlign="justify" variant="body1" paragraph={true}>
+//           You can download the results of your customized searches for further analysis.
+//         </Typography>
+//       </Section>
+//     </Stack>
+//   );
+// }
--- src/components/narrative/time-series/TimeSeries.tsx
+++ src/components/narrative/time-series/TimeSeries.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useContext } from 'react';
 import {
   Box,
   Container,
@@ -16,26 +16,18 @@ import type { TimeSeries as TimeSeriesType } from '../../../types';
 import HighChart from './HighChart';
 import Header from '../../Header';
 import Toolbar from './Toolbar';
-
-type CombinedTimeSeries = {
-  meta: {
-    from: Date;
-    weightsStats: any; // TODO Specify the type for weightsStats
-  };
-  price: {
-    color: string;
-    points: any[]; // TODO Specify the type for points
-  };
-  weights: any[]; // TODO Specify the type for weights
-};
+import './TimeSeries.css';
+import Widgetbar from './Widgetbar';
+import Bottombar from './Bottombar';
+import LoadingChart from './LoadingChart';
+import { SeriesContext } from './../../../context/SeriesContext';
+import { LoadingContext } from './../../../context/LoadingContext';
 
 const TimeSeries = () => {
   const theme = useTheme();
 
-  const [combinedTimeSeries, setCombinedTimeSeries] = React.useState<CombinedTimeSeries | undefined>();
-  const [symbolTimeSeries, setSymbolTimeSeries] = React.useState<TimeSeriesType>(); // TODO Specify the type
-  const [textTimeSeries, setTextTimeSeries] = React.useState<any[]>([]); // TODO Specify the type
-  const [isTextLoading, setTextLoading] = React.useState(false);
+  const { combinedTimeSeries, setCombinedTimeSeries, symbolTimeSeries, setSymbolTimeSeries, textTimeSeries, setTextTimeSeries } = useContext(SeriesContext);
+  const { isTextLoading, setTextLoading } = useContext(LoadingContext);
 
   React.useMemo(() => {
     const isSymbolsLoaded = symbolTimeSeries !== undefined;
@@ -68,80 +60,94 @@ const TimeSeries = () => {
   };
 
   return (
-    <div className="flex flex-col h-screen">
-      <Header/>
+    <div className="tv-container">
+      <Header />
       <main className="flex flex-grow">
         <Toolbar />
+        <div className="dashboard">
+          <div className="charts tv-box">
+            {combinedTimeSeries  
+              ? <HighChart timeSeries={combinedTimeSeries} />
+              : ( 
+                !isTextLoading
+                ? <Intro />
+                : <LoadingChart />
+              )
+            }
+          </div>
+          <Bottombar />
+        </div>
+        <Widgetbar onTimeSeries={setSymbolTimeSeries}/>
       </main>
     </div>
   );
   
-  // return (
-  //   <>
-  //     <Header/>
-  //     <Grid container direction="row" justifyContent="space-between">
-  //       <Grid container direction="column" item sm={12} md={9}>
-  //         <Grid container item minHeight={750} flexDirection="column">
-  //           {combinedTimeSeries && <Chart timeSeries={combinedTimeSeries} />}
-  //           {combinedTimeSeries && <HighChart timeSeries={combinedTimeSeries} />}
-  //           {!combinedTimeSeries && !isTextLoading && (
-  //             <Box sx={{ pl: 2, pr: 8, pt: 2, my: 'auto' }}>
-  //               <Intro />
-  //             </Box>
-  //           )}
-  //           {!combinedTimeSeries && isTextLoading && (
-  //             <Container
-  //               maxWidth={'sm'}
-  //               sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, justifyContent: 'center' }}
-  //             >
-  //               <EngineeringIcon sx={{ alignSelf: 'center', color: theme.palette.action.disabled, fontSize: 120 }} />
-  //               <Typography
-  //                 color={theme.palette.action.disabled}
-  //                 component="p"
-  //                 gutterBottom
-  //                 textAlign="center"
-  //                 variant="h5"
-  //               >
-  //                 We're crunching the numbers!
-  //               </Typography>
-  //               <Typography color={theme.palette.action.disabled} component="p" textAlign="center">
-  //                 Please be patient as this may take a few seconds.
-  //               </Typography>
-  //             </Container>
-  //           )}
-  //         </Grid>
-  //         <Grid item>
-  //           <SearchInput onTimeSeries={handleTextTimeSeries} />
-  //         </Grid>
-  //         {textTimeSeries.length > 0 && (
-  //           <Grid
-  //             item
-  //             component="ol"
-  //             sx={{
-  //               listStyle: 'none',
-  //               p: 0,
-  //               ml: 2,
-  //               mr: 8,
-  //             }}
-  //           >
-  //             {textTimeSeries.map((tts) => (
-  //               <Summary
-  //                 key={tts.text}
-  //                 text={tts.text}
-  //                 color={tts.color}
-  //                 component="li"
-  //                 sx={{ mb: 2 }}
-  //               />
-  //             ))}
-  //           </Grid>
-  //         )}
-  //       </Grid>
-  //       <Grid item sm="auto" md={3}>
-  //         <Symbols onTimeSeries={setSymbolTimeSeries} />
-  //       </Grid>
-  //     </Grid>
-  //   </>
-  // );
+//   return (
+//     <>
+//       <Header/>
+//       <Grid container direction="row" justifyContent="space-between">
+//         <Grid container direction="column" item sm={12} md={9}>
+//           <Grid container item minHeight={750} flexDirection="column">
+//             {/* {combinedTimeSeries && <Chart timeSeries={combinedTimeSeries} />} */}
+//             {combinedTimeSeries && <HighChart timeSeries={combinedTimeSeries} />}
+//             {!combinedTimeSeries && !isTextLoading && (
+//               <Box sx={{ pl: 2, pr: 8, pt: 2, my: 'auto' }}>
+//                 <Intro />
+//               </Box>
+//             )}
+//             {!combinedTimeSeries && isTextLoading && (
+//               <Container
+//                 maxWidth={'sm'}
+//                 sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, justifyContent: 'center' }}
+//               >
+//                 <EngineeringIcon sx={{ alignSelf: 'center', color: theme.palette.action.disabled, fontSize: 120 }} />
+//                 <Typography
+//                   color={theme.palette.action.disabled}
+//                   component="p"
+//                   gutterBottom
+//                   textAlign="center"
+//                   variant="h5"
+//                 >
+//                   We're crunching the numbers!
+//                 </Typography>
+//                 <Typography color={theme.palette.action.disabled} component="p" textAlign="center">
+//                   Please be patient as this may take a few seconds.
+//                 </Typography>
+//               </Container>
+//             )}
+//           </Grid>
+//           <Grid item>
+//             <SearchInput onTimeSeries={handleTextTimeSeries} />
+//           </Grid>
+//           {textTimeSeries.length > 0 && (
+//             <Grid
+//               item
+//               component="ol"
+//               sx={{
+//                 listStyle: 'none',
+//                 p: 0,
+//                 ml: 2,
+//                 mr: 8,
+//               }}
+//             >
+//               {textTimeSeries.map((tts) => (
+//                 <Summary
+//                   key={tts.text}
+//                   text={tts.text}
+//                   color={tts.color}
+//                   component="li"
+//                   sx={{ mb: 2 }}
+//                 />
+//               ))}
+//             </Grid>
+//           )}
+//         </Grid>
+//         <Grid item sm="auto" md={3}>
+//           <Symbols onTimeSeries={setSymbolTimeSeries} />
+//         </Grid>
+//       </Grid>
+//     </>
+//   );
 };
 
 export default TimeSeries;
